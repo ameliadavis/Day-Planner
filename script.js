b67f71f0e9244777c4649905257700b77b53685c
Amelia Davis <ameliaaltman22@gmail.com>
2020-01-19 22:01:46 -0700

    corrected Event handler for click

--- script.js
+++ script.js
@@ -1,49 +1,63 @@
 // Declare global variables
 var currentDay = document.getElementById("currentDay");
 var todaysDate = moment().format('MMMM Do YYYY'); 
-var hourEl = document.getElementById("hour");
-var hourDataEl = $("#hour").attr("data-time");
+// var hourEl = document.getElementById("hour");
+var hourEl = $("#hour");
+// var hourDataEl = $("#hour").attr("data-time");
 var saveButton = document.getElementById("saveBtn");
-var descriptionEl = document.getElementById("description");
+// var descriptionEl = $("#description").val();
 var timeNow = moment().format('LTS');
-console.log("this should be the data on time " + hourDataEl);
+// console.log("this should be the data on time " + hourDataEl);
+
+$("#saveBtn").on("click", function(){
+    console.log("register the click");
+    var descriptionEl = $("input").val();
+   
+    console.log(descriptionEl);
+    save();
+
 
 // function for save button to add content to local storage 
 function save() {
+    event.preventDefault();
+    localStorage.setItem("this is a test", "this is only a test");
+    localStorage.setItem("this is the description", descriptionEl);
+ };
+});
 
-    // var currentDescription = descriptionEl(this);
-    console.log("this",this);
-    localStorage.setItem("to do",currentDescription);// need to grab this input text and date, 
-}
 //function to turn hours colors based on time 
 function timePassing (){
-    for(var i = 0; i < $(".textarea").length ;i++){ 
-        // not sure how to get this to cycle through all the hour elements on the page. 
+    for(var i = 0; i < $("#description").length ;i++){ // cycle thorugh all the elements on the page
         console.log(i);
         if(hourEl[i] < timeNow){
-            $(".textarea").addClass("past");
+            $("#description").addClass("past");
             console.log("past");
         } else if (hourEl[i] = timeNow) {
-            $(".textarea").addClass("present");
+            $("#description").addClass("present");
             console.log("present");
         } else {
-            $(".textarea").addClass("future");
+            $("#description").addClass("future");
             console.log("future");
         };
     };
-}
+    };
 
 
+// $(document).ready(function(){
+// document.getElementById("description1").innerHTML = localStorage.getItem("lastname");
+// });
 
+// $("input").keyup("click",function(){
+//     var value = $(this).val();
+//     $("div").text(value);
+//     console.log(value);
+// } )
 
-
-//set date code will go at the end of the script to load when page loads
-console.log("todays date variable",todaysDate);
-currentDay.innerHTML = todaysDate;
-
-// event listener for save button
-$(saveButton).on("click",".data-value", save);
 
 // current time
 console.log("this is the current time", moment().format('LTS'));
+
+//set date code will go at the end of the script to load when page loads
+// console.log("todays date variable",todaysDate);
+currentDay.innerHTML = todaysDate;
 timePassing();
