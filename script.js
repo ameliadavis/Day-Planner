c0071003af6486105da4a18107d2b3d8c6015ef6
Amelia Davis <ameliaaltman22@gmail.com>
2020-01-20 23:19:51 -0700

    corrected issue with color Highlight extending to all rows

--- script.js
+++ script.js
@@ -1,63 +1,72 @@
 // Declare global variables
 var currentDay = document.getElementById("currentDay");
 var todaysDate = moment().format('MMMM Do YYYY'); 
-// var hourEl = document.getElementById("hour");
-var hourEl = $("#hour");
-// var hourDataEl = $("#hour").attr("data-time");
+var hourEl = $("#hour").data("time");
 var saveButton = document.getElementById("saveBtn");
-// var descriptionEl = $("#description").val();
-var timeNow = moment().format('LTS');
-// console.log("this should be the data on time " + hourDataEl);
+// var descriptionEl = document.getElementById("description");
+var description = $(".description");
+var descriptionEl = $(".description").val();
+var timeNow = moment().format('H');// this is in military time
+var table = $("#table");
 
+console.log("this should be the current hour " + timeNow);
+console.log("this is the hour element " + hourEl);
+
+//function to turn hours colors based on time 
+function timePassing (){
+    // for(var i = 0; i < $(".hour").length + 1; i++){ // cycle thorugh all the elements on the page
+        $(".hour").each(function() {
+            // console.log(i);
+        var description = $(".hour").length;
+        console.log(description);
+        hourEl = parseInt($(this).attr("id"));
+        console.log(hourEl);
+        if(hourEl < timeNow){
+            $(this).addClass("past");
+            console.log("past");
+            console.log(this);
+        } else if (hourEl === timeNow) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            console.log("present");
+        } else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+            console.log("future");
+        };
+     });
+    };
+    // save button listener
 $("#saveBtn").on("click", function(){
     console.log("register the click");
     var descriptionEl = $("input").val();
-   
-    console.log(descriptionEl);
-    save();
+    console.log(this);
 
+    save();
 
 // function for save button to add content to local storage 
 function save() {
     event.preventDefault();
     localStorage.setItem("this is a test", "this is only a test");
-    localStorage.setItem("this is the description", descriptionEl);
+    localStorage.setItem("this is the description", descriptionEl );
  };
 });
 
-//function to turn hours colors based on time 
-function timePassing (){
-    for(var i = 0; i < $("#description").length ;i++){ // cycle thorugh all the elements on the page
-        console.log(i);
-        if(hourEl[i] < timeNow){
-            $("#description").addClass("past");
-            console.log("past");
-        } else if (hourEl[i] = timeNow) {
-            $("#description").addClass("present");
-            console.log("present");
-        } else {
-            $("#description").addClass("future");
-            console.log("future");
-        };
-    };
-    };
+// this will print the items saved in local storage back to the screen 
+$(document).ready(function(){
+document.getElementsByClassName("hour").innerHTML = localStorage.getItem("this is the description");
+});
 
-
-// $(document).ready(function(){
-// document.getElementById("description1").innerHTML = localStorage.getItem("lastname");
-// });
-
-// $("input").keyup("click",function(){
-//     var value = $(this).val();
-//     $("div").text(value);
-//     console.log(value);
-// } )
-
+// from solution 
+// $(document).ready(function() {  // listen for save button clicks  $(".saveBtn").on("click", function() {    // get nearby values    var value = $(this).siblings(".description").val();    var time = $(this).parent().attr("id");
+    // save in localStorage    localStorage.setItem(time, value);  });
 
 // current time
 console.log("this is the current time", moment().format('LTS'));
 
-//set date code will go at the end of the script to load when page loads
-// console.log("todays date variable",todaysDate);
+// set the date at the top of the page
 currentDay.innerHTML = todaysDate;
+
+// run function 
 timePassing();
